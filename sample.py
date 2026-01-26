70cebec9cc3db2667c695b59339819a4894a562f
Afjal Tyagi <afzalpc287@gmail.com>
2026-01-26 13:11:53 +0530

    Adding Comments

--- sample.py
+++ sample.py
@@ -1,13 +1,17 @@
+# Sample of my First Project
+# Importing Librarys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mlt
 import seaborn as sns
+# Using While Loop for Enter Class
 valid_class=[9,10,11,12]
 print('Hello Students...Please Enter Your Class')
 classes=int(input("class:"))
 while classes not in valid_class:
      print('Enter Only 9 to 12') 
      classes=int(input("class:"))
+# 9 or 10 class Students enter Name and Subject     
 print('Your Class is :',classes)
 if classes == 9 or classes== 10:
     print('Now Please Enter Your Details')
@@ -17,6 +21,7 @@ if classes == 9 or classes== 10:
     social_science=int(input('Social Science marks:'))
     hindi=int(input('Hindi marks:'))
     english=int(input('English marks:'))
+    # Convert input to Dataframe
     subjects={'Name':[Name],
               'Maths':[Maths_Marks],
               'Science':[science],
@@ -25,10 +30,13 @@ if classes == 9 or classes== 10:
               'English':[english]}
     df=pd.DataFrame(subjects)
     print(df)
+    # Create a Plot by Using SeaBorn or Matplotlib
     sns.catplot(df,kind='bar')
     mlt.title(f'{Name}-Subjects Progress Comparison')
     mlt.show()
+    # Taking Input from Student of Interesting Subject
     interest=input('Enter Your Interested Subjects:').lower()
+    # Suggest Career Option to Student Basis on Interested Subject
     valid_subject=['maths','science','social science','accounts']
     while interest not in valid_subject:
         print('Enter Valid Subject')
@@ -38,7 +46,8 @@ if classes == 9 or classes== 10:
     elif interest == 'social science':
         print("Arts Stream is Best for you in 12th Class")
     elif interest == 'maths' or interest == 'accounts' :
-        print('Commerce Stream is Best For You in 12th Class')             
+        print('Commerce Stream is Best For You in 12th Class')
+    # Adding Grade of Student Marks                 
     grade=(Maths_Marks+science+social_science+hindi+english)/5    
     if grade>=90:
         print("A Grade:",grade,'%')
@@ -50,10 +59,14 @@ if classes == 9 or classes== 10:
         print('D Grade',grade,'%')    
     else:
         print("FAIL")            
+#  11 or 12 class Start         
 elif classes == 11 or classes== 12:
     print('Now PLease Enter your Stream')
+    # Taking input sream for better Analyse
     stream=str(input('stream:'))
+    # Science Stream Start
     if stream =="Science" or stream== 'science':
+        #  11 or 12 class Science Stream Students enter Name and Subject
         print('Now Please Enter Your Details')
         Name=str(input("Name:"))
         Maths_Marks=int(input("Maths Marks:"))
@@ -62,6 +75,7 @@ elif classes == 11 or classes== 12:
         biology=int(input('Biology Mraks:'))
         hindi=int(input('Hindi Marks:'))
         english=int(input('English Marks:'))
+         # Convert input to Dataframe
         subjects={'Name':[Name],
               'Maths':[Maths_Marks],
               'Physics':[physics],
@@ -71,11 +85,14 @@ elif classes == 11 or classes== 12:
               'English':[english]}
         df=pd.DataFrame(subjects)
         print(df)
+        # Create a Plot by Using SeaBorn or Matplotlib
         sns.catplot(df,kind='bar')
         mlt.title(f'{Name}-Subjects Progress Comparison')
         mlt.show()
+        # Taking Input from Student of Interesting Subject
         interest=input('Enter Your Interested Subjects:').lower()
         valid_subject=['maths','physics','chemistry','biology','hindi','english']
+        # Suggest Career Option to Student Basis on Interested Subject
         while interest not in valid_subject:
             print('Enter Valid Subject')
             interest=input('Enter Your Interested Subjects:').lower()
@@ -86,7 +103,8 @@ elif classes == 11 or classes== 12:
                   '\n','You Also Have More Options like:D.Pharma,B.Phharma, and for more info search on internet')
         elif interest in  ['hindi','english']:
             print('Just Do CUET and Get Addmission in', interest,'hounors and Become Lecturare',
-                  '\n','You Also Have More Options like:D.Pharma,B.Phharma, and for more info search on internet' )            
+                  '\n','You Also Have More Options like:D.Pharma,B.Phharma, and for more info search on internet' )
+        # Adding Grade of Student Marks                  
         grade=(Maths_Marks+physics+chemistry+biology+hindi+english)/6    
         if grade>=90:
             print("A Grade:",grade,'%')
@@ -97,9 +115,11 @@ elif classes == 11 or classes== 12:
         elif grade>=35 and grade<50:
             print('D Grade',grade,'%')    
         else:
-            print("FAIL")              
+            print("FAIL") 
+    # Arts Stream Start                     
     elif stream == "Arts" or stream== 'arts':     
         print('Now Please Enter Your Details')
+        # 11 or 12 class Arts Stream Students enter Name and Subject
         Name=str(input("Name:"))
         Geography=int(input("Geography Marks:"))
         history=int(input("History Marks:"))
@@ -108,6 +128,7 @@ elif classes == 11 or classes== 12:
         economics=int(input('Economics Marks:'))
         hindi=int(input('Hindi Marks:'))
         english=int(input('English Marks:'))
+        # Convert input to Dataframe
         subjects={'Name':[Name],
               'Geography':[Geography],
               'History':[history],
@@ -118,11 +139,14 @@ elif classes == 11 or classes== 12:
               'English':[english]}
         df=pd.DataFrame(subjects)
         print(df)
+        # Create a Plot by Using SeaBorn or Matplotlib
         sns.catplot(df,kind='bar')
         mlt.title(f'{Name}-Subjects Progress Comparison')
         mlt.show()
+        # Taking Input from Student of Interesting Subject
         interest=input('Enter Your Interested Subjects:').lower()
         valid_subject=['geography','history','political science','sociology','economics','hindi','english']
+        # Suggest Career Option to Student Basis on Interested Subject
         while interest not in valid_subject:
             print('Enter Valid Subject')
             interest=input('Enter Your Interested Subjects:').lower()
@@ -132,6 +156,7 @@ elif classes == 11 or classes== 12:
         elif interest in ['sociology','economics','hindi','english']:
             print('Just Do CUET and Get Addmission in', interest,'hounors and Become Lecturare'
                    '\n','You Also Have More Options like:BCA,BBA,IT Feild and for more info search on internet')
+        # Adding Grade of Student Marks   
         grade=(Geography+history+political_science+sociology+economics+hindi+english)/7   
         if grade>=90:
             print("A Grade:",grade,'%')
@@ -142,9 +167,11 @@ elif classes == 11 or classes== 12:
         elif grade>=35 and grade<50:
             print('D Grade',grade,'%')    
         else:
-            print("FAIL")              
+            print("FAIL")
+    # Arts Stream Start                       
     elif stream=="Commerce" or stream=='commerce':     
         print('Now Please Enter Your Details')
+        # 11 or 12 class Commerce Stream Students enter Name and Subject
         Name=str(input("Name:"))
         Accounts=int(input("Accounts Marks:"))
         Maths_Marks=int(input("Maths Marks:"))
@@ -154,6 +181,7 @@ elif classes == 11 or classes== 12:
         legal_study=int(input('Legal Studies Marks:'))
         hindi=int(input('Hindi Marks:'))
         english=int(input('English Marks:')) 
+        # Convert input to Dataframe
         subjects={'Name':[Name],
               'Maths':[Maths_Marks],
               'Accounts':[Accounts],
@@ -164,12 +192,15 @@ elif classes == 11 or classes== 12:
               'Hindi':[hindi],
               'English':[english]}
         df=pd.DataFrame(subjects)
-        print(df) 
+        print(df)
+        # Create a Plot by Using SeaBorn or Matplotlib 
         sns.catplot(df,kind='bar')
         mlt.title(f'{Name}-Subjects Progress Comparison')
         mlt.show()
+        # Taking Input from Student of Interesting Subject
         interest=input('Enter Your Interested Subjects:').lower()
         valid_subject=['maths','accounts','business','economics','hindi','english','enterpreneur','legal study']
+        # Suggest Career Option to Student Basis on Interested Subject
         while interest not in valid_subject:
             print('Enter Valid Subject')
             interest=input('Enter Your Interested Subjects:').lower()
@@ -178,10 +209,11 @@ elif classes == 11 or classes== 12:
                   '\n','You Also Have More Options like:B.Com,BBA, and for more info search on internet')
         elif interest in ['enterpreneur']:
             print("Just Do Enterpreneur Course and Become an Enterprenuer"
-                  '\n','You Also Have More Options like:B.Com,BBA, and for more info search on internet')
+                  '\n','You Also Have More Options like:B.Com,BBA, and for more info s# Adding Grade of Student Marks earch on internet')
         elif interest in  ['hindi','english']:
             print('Just Do CUET and Get Addmission in', interest,'hounors and Become Lecturare'
-                  '\n','You Also Have More Options like:B.Com,BBA, and for more info search on internet')             
+                  '\n','You Also Have More Options like:B.Com,BBA, and for more info search on internet') 
+        # Adding Grade of Student Marks                 
         grade=(Accounts+Maths_Marks+business+economics+enterpre+legal_study+hindi+english)/8   
         if grade>=90:
             print("A Grade:",grade,'%')
